4df68c9e5849f5528f45b2ba944635098b28d82d
EC2 Default User <ec2-user@ip-172-31-82-111.ec2.internal>
2020-10-11 22:08:37 +0000

    Added print message

--- src/main/java/call/hello/App.java
+++ src/main/java/call/hello/App.java
@@ -4,15 +4,21 @@ public class App
 {
 
     private final String message = "Hello World from Java Maven Project!";
+    private final String message1 = "Hello World from Java and second try!";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+	System.out.println(new App().getMessage1());
     }
 
     private final String getMessage() {
         return message;
+    }
+
+    private final String getMessage1() {
+        return message1;
     }
 
 }
